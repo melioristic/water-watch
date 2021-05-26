cb2857393755a643b4d977e768ca604fd105f9ed
Melioristic <itsmohitanand@gmail.com>
2021-05-26 08:21:03 +0200

    shape files for git

---
+++ Notes.md
@@ -0,0 +1,8 @@
+The catchments are numbered from 1-5 from left to right 
+
+And then visually the center coordinate is found for each of the shape file. 
+
+Catchment - I - 76.2, 12.5      
+Catchment - II - 76.3, 12
+Catchment - III - 77, 12
+Catchment - IV - 77.6, 12.3
--- WW/io.py
+++ WW/io.py
@@ -5,12 +5,14 @@ from shapely.geometry import mapping
 
 
 temp_max_b = 'data/temp_max/Maxtemp_MaxT_2011.GRD'
-shp_file = 'data/shapefiles/hydrobasin6_Bangalore_clip_HYBAS_ID_4061139430.0.shp'
+shp_file = 'data/shapefiles/hydrobasin6_Bangalore_clip_HYBAS_ID_4061140230.0.shp'
 
 xds = rxr.open_rasterio(
     "data/temp_max/Maxtemp_MaxT_2011.nc",
     masked=True,
 )
+
+
 xds.rio.set_crs(4326)
 xds[0].plot()
 plt.savefig('plots/temp.png')
@@ -18,14 +20,14 @@ plt.close()
 
 
 geodf = gpd.read_file(shp_file)
-print(geodf.crs)
 geodf.plot()
 plt.savefig('plots/shp.png')
 plt.close()
 
-clipped = xds.rio.clip(geodf.geometry.apply(mapping), geodf.crs)
+clipped = xds.rio.clip(geodf.geometry.apply(mapping), "EPSG:4326")
+print(clipped)
 
-clipped.plot()
+clipped[0].plot()
 plt.savefig('plots/clip.png')
 plt.close()
 
